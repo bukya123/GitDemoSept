b6b94ab963483258375d7959959c3f758aa7f75b
Bukya123 <ashritha71557@gmail.com>
2024-08-24 00:58:39 +0530

    added feature2

--- src/MaxSpeedComparator.java
+++ src/MaxSpeedComparator.java
@@ -1,10 +1,7 @@
 public class MaxSpeedComparator {
     public static void main(String[] args) {
         System.out.print("Hi");
-        System.out.print("Hi");
-        System.out.print("Hi");
-        System.out.print("Hi");
-
+        System.out.print("Hello");
     }
 
 }
