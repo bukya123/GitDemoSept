c10deed2352873bbf400da297b019a337a30a754
Bukya123 <ashritha71557@gmail.com>
2024-08-24 00:51:20 +0530

    added feature branch

---
+++ src/MaxSpeedComparator.java
@@ -0,0 +1,10 @@
+public class MaxSpeedComparator {
+    public static void main(String[] args) {
+        System.out.print("Hi");
+        System.out.print("Hi");
+        System.out.print("Hi");
+        System.out.print("Hi");
+
+    }
+
+}
